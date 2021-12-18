7bbd1ab63878457d42564fd5b08c2c32b77d2655
fustom <fustom@gmail.com>
2021-12-18 18:31:10 +0000

    Code beauty update with string constants

--- custom_components/ariston/device.py
+++ custom_components/ariston/device.py
@@ -4,8 +4,18 @@ from __future__ import annotations
 import logging
 
 from typing import Any
+from ctypes import Array
 
-from .ariston import AristonAPI, PlantMode, ZoneMode
+from .ariston import (
+    AristonAPI,
+    DeviceAttribute,
+    DeviceFeatures,
+    DeviceProperties,
+    PlantMode,
+    PropertyType,
+    ThermostatProperties,
+    ZoneMode,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -17,12 +27,12 @@ class AristonDevice:
         self.api = api
 
         """Device properites"""
-        self.gw_id = device["gwId"]
-        self.gw_serial = device["gwSerial"]
-        self.plant_name = device["plantName"]
-        self.gw_fw_ver = device["gwFwVer"]
-        self.gw_sys_type = device["gwSysType"]
-        self.model = device["plantName"]
+        self.gw_id = device[DeviceAttribute.GW_ID]
+        self.gw_serial = device[DeviceAttribute.GW_SERIAL]
+        self.plant_name = device[DeviceAttribute.PLANT_NAME]
+        self.gw_fw_ver = device[DeviceAttribute.GW_FW_VER]
+        self.gw_sys_type = device[DeviceAttribute.GW_SYS_TYPE]
+        self.model = device[DeviceAttribute.PLANT_NAME]
         self.location = "en-US"
 
         self.thermostats = []
@@ -54,7 +64,7 @@ class AristonDevice:
         self.features = AristonDeviceFeatures(
             await self.api.async_get_features_for_device(self.gw_id)
         )
-        thermostats = []
+        thermostats: Array[Thermostat] = []
         for zone in self.features.zones:
             thermostats.append(Thermostat(self, zone))
         self.thermostats = thermostats
@@ -62,33 +72,59 @@ class AristonDevice:
     async def async_update_state(self) -> None:
         """Update the device states from the cloud"""
         self.data = await self.api.async_update_device(
-            self.gw_id, self.features.json, self.location
+            self.gw_id, self.features.data, self.location
         )
 
-        self.plant_mode = self.get_item_by_id("PlantMode", "value")
-        self.plant_modes = self.get_item_by_id("PlantMode", "options")
-        self.plant_mode_texts = self.get_item_by_id("PlantMode", "optTexts")
-        self.is_flame_on = self.get_item_by_id("IsFlameOn", "value")
-        self.ch_flow_setpoint_temp = self.get_item_by_id("ChFlowSetpointTemp", "value")
+        self.plant_mode = self.get_item_by_id(
+            DeviceProperties.PLANT_MODE, PropertyType.VALUE
+        )
+        self.plant_modes = self.get_item_by_id(
+            DeviceProperties.PLANT_MODE, PropertyType.OPTIONS
+        )
+        self.plant_mode_texts = self.get_item_by_id(
+            DeviceProperties.PLANT_MODE, PropertyType.OPT_TEXTS
+        )
+        self.is_flame_on = self.get_item_by_id(
+            DeviceProperties.IS_FLAME_ON, PropertyType.VALUE
+        )
+        self.ch_flow_setpoint_temp = self.get_item_by_id(
+            DeviceProperties.CH_FLOW_SETPOINT_TEMP, PropertyType.VALUE
+        )
         self.ch_flow_setpoint_temp_unit = self.get_item_by_id(
-            "ChFlowSetpointTemp", "unit"
+            DeviceProperties.CH_FLOW_SETPOINT_TEMP, PropertyType.UNIT
         )
         self.heating_circuit_pressure = self.get_item_by_id(
-            "HeatingCircuitPressure", "value"
+            DeviceProperties.HEATING_CIRCUIT_PRESSURE, PropertyType.VALUE
         )
         self.heating_circuit_pressure_unit = self.get_item_by_id(
-            "HeatingCircuitPressure", "unit"
+            DeviceProperties.HEATING_CIRCUIT_PRESSURE, PropertyType.UNIT
         )
 
         if self.features.has_boiler:
-            self.dhw_temp = self.get_item_by_id("DhwTemp", "value")
-            self.dhw_temp_min = self.get_item_by_id("DhwTemp", "min")
-            self.dhw_temp_max = self.get_item_by_id("DhwTemp", "max")
-            self.dhw_temp_step = self.get_item_by_id("DhwTemp", "step")
-            self.dhw_temp_unit = self.get_item_by_id("DhwTemp", "unit")
-            self.dhw_mode = self.get_item_by_id("DhwMode", "value")
-            self.dhw_modes = self.get_item_by_id("DhwMode", "options")
-            self.dhw_mode_texts = self.get_item_by_id("DhwMode", "optTexts")
+            self.dhw_temp = self.get_item_by_id(
+                DeviceProperties.DHW_TEMP, PropertyType.VALUE
+            )
+            self.dhw_temp_min = self.get_item_by_id(
+                DeviceProperties.DHW_TEMP, PropertyType.MIN
+            )
+            self.dhw_temp_max = self.get_item_by_id(
+                DeviceProperties.DHW_TEMP, PropertyType.MAX
+            )
+            self.dhw_temp_step = self.get_item_by_id(
+                DeviceProperties.DHW_TEMP, PropertyType.STEP
+            )
+            self.dhw_temp_unit = self.get_item_by_id(
+                DeviceProperties.DHW_TEMP, PropertyType.UNIT
+            )
+            self.dhw_mode = self.get_item_by_id(
+                DeviceProperties.DHW_MODE, PropertyType.VALUE
+            )
+            self.dhw_modes = self.get_item_by_id(
+                DeviceProperties.DHW_MODE, PropertyType.OPTIONS
+            )
+            self.dhw_mode_texts = self.get_item_by_id(
+                DeviceProperties.DHW_MODE, PropertyType.OPT_TEXTS
+            )
 
         for thermostat in self.thermostats:
             await thermostat.async_update_state()
@@ -143,24 +179,46 @@ class Thermostat:
         self.data = await self.device.api.async_update_thermostat(
             self.device.gw_id,
             self.zone,
-            self.device.features.json,
+            self.device.features.data,
             self.device.location,
         )
 
-        self.measured_temp = self.get_item_by_id("ZoneMeasuredTemp", "value")
-        self.measured_temp_unit = self.get_item_by_id("ZoneMeasuredTemp", "unit")
-        self.measured_temp_decimals = self.get_item_by_id(
-            "ZoneMeasuredTemp", "decimals"
+        self.measured_temp = self.get_item_by_id(
+            ThermostatProperties.ZONE_MEASURED_TEMP, PropertyType.VALUE
         )
-        self.desired_temp = self.get_item_by_id("ZoneDesiredTemp", "value")
-        self.comfort_temp = self.get_item_by_id("ZoneComfortTemp", "value")
-        self.comfort_temp_min = self.get_item_by_id("ZoneComfortTemp", "min")
-        self.comfort_temp_max = self.get_item_by_id("ZoneComfortTemp", "max")
-        self.comfort_temp_step = self.get_item_by_id("ZoneComfortTemp", "step")
-        self.mode = self.get_item_by_id("ZoneMode", "value")
-        self.modes = self.get_item_by_id("ZoneMode", "options")
-        self.heat_request = self.get_item_by_id("ZoneHeatRequest", "value")
-        self.zone_economy_temp = self.get_item_by_id("ZoneEconomyTemp", "value")
+        self.measured_temp_unit = self.get_item_by_id(
+            ThermostatProperties.ZONE_MEASURED_TEMP, PropertyType.UNIT
+        )
+        self.measured_temp_decimals = self.get_item_by_id(
+            ThermostatProperties.ZONE_MEASURED_TEMP, PropertyType.DECIMALS
+        )
+        self.desired_temp = self.get_item_by_id(
+            ThermostatProperties.ZONE_DESIRED_TEMP, PropertyType.VALUE
+        )
+        self.comfort_temp = self.get_item_by_id(
+            ThermostatProperties.ZONE_COMFORT_TEMP, PropertyType.VALUE
+        )
+        self.comfort_temp_min = self.get_item_by_id(
+            ThermostatProperties.ZONE_COMFORT_TEMP, PropertyType.MIN
+        )
+        self.comfort_temp_max = self.get_item_by_id(
+            ThermostatProperties.ZONE_COMFORT_TEMP, PropertyType.MAX
+        )
+        self.comfort_temp_step = self.get_item_by_id(
+            ThermostatProperties.ZONE_COMFORT_TEMP, PropertyType.STEP
+        )
+        self.mode = self.get_item_by_id(
+            ThermostatProperties.ZONE_MODE, PropertyType.VALUE
+        )
+        self.modes = self.get_item_by_id(
+            ThermostatProperties.ZONE_MODE, PropertyType.OPTIONS
+        )
+        self.heat_request = self.get_item_by_id(
+            ThermostatProperties.ZONE_HEAT_REQUEST, PropertyType.VALUE
+        )
+        self.zone_economy_temp = self.get_item_by_id(
+            ThermostatProperties.ZONE_ECONOMY_TEMP, PropertyType.VALUE
+        )
 
     async def async_set_temperature(self, temperature: float):
         """Set comfort temperature wrapper"""
@@ -187,7 +245,7 @@ class AristonDeviceFeatures:
     """Class representing a device feature properties."""
 
     def __init__(self, features: dict[str, Any]) -> None:
-        self.json = features
-        self.has_boiler = features["hasBoiler"]
-        self.zones = features["zones"]
-        self.dhw_mode_changeable = features["dhwModeChangeable"]
+        self.data = features
+        self.has_boiler = features[DeviceFeatures.HAS_BOILER]
+        self.zones = features[DeviceFeatures.ZONES]
+        self.dhw_mode_changeable = features[DeviceFeatures.DHW_MODE_CHANGEABLE]
