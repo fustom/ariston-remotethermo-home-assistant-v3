57cbe9182fa548edc5b2835d362205471adaf32d
fustom <fustom@gmail.com>
2023-02-22 23:10:39 +0000

    Replace async_setup_platforms with async_forward_entry_setups

--- custom_components/ariston/__init__.py
+++ custom_components/ariston/__init__.py
@@ -112,7 +112,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN][entry.unique_id][ENERGY_COORDINATOR] = energy_coordinator
         await energy_coordinator.async_config_entry_first_refresh()
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(update_listener))
 
